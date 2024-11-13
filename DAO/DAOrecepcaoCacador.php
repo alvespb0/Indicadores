3ce5a98199166d5ebae2c357b8d57d9c2e2c7fb4
Your Name <you@example.com>
2024-11-13 15:36:35 -0300

    Feito a DAO recepcao Cacador

--- DAO/DAOrecepcaoCacador.php
+++ DAO/DAOrecepcaoCacador.php
@@ -1,6 +1,82 @@
 <?php
 namespace DAO;
 
-require_once(../models/recepcaoCacador.php);
-use models\recepcaoCacador;
+require_once('../models/recepcaoCacador.php');
+use models\RecepcaoCacador;
+
+/**
+ * Esta classe é responsável por fazer a couminicação entre o banco de dados,
+ * provendo as funções CRUD para a tabela de recepção caçador
+ * @author Arthur Alves
+ * @package DAO
+ */
+
+class DAORecepcaoCacador{
+    /**
+     * Inclui os indicadores
+     * @param int $clinicos
+     * @param int $audiometria
+     * @param int $acuidade
+     * @param int $fonoaudiologia
+     * @param int $ecg
+     * @param int $eeg
+     * @param int $espirometria
+     * @param int $raio_x
+     * @param int $av_psicossocial
+     * @param int $av_medica
+     * @param int $laboratoriais
+     * @param int $pericias
+     * @param date $data 
+     * @return TRUE|Exception
+     */
+    public function incluirIndicador($clinicos, $audiometria, $acuidade,
+    $fonoaudiologia, $ecg, $eeg, $espirometria, $raio_x, $av_psicossocial, $av_medica, $laboratoriais, $pericias, $data){
+        try{
+            $conexaoDB = $this->conectarBanco();
+        }catch(\Exception $e){
+            die($e->getMessage())
+        }
+        $sqlInsert = $conexaoDB->prepare("insert into recepcao_cacador
+        (data, clinicos, audiometria, acuidade, fonoaudiologia_clinica, ecg, eeg,
+        espirometria, raio_x, av_psicossocial, av_medica, laboratoriais, pericias) values (?,?,?,?,?,?,?,?,?,?,?,?");
+        $sqlInsert->bind_param("siiiiiiiiiiii",$data, $clinicos, $audiometria, $acuidade, 
+        $fonoaudiologia, $ecg, $eeg, $espirometria, $raio_x, $av_psicossocial, $av_medica, $laboratoriais, $pericias);
+        $sqlInsert->execute();
+
+        if(!$sqlInsert->error){
+            $retorno = TRUE;
+        }else{
+            throw new \Exception ("não foipossível incluir indicador");
+            die;
+        }
+        $conexaoDB->close();
+        $sqlInsert->close();
+        return $retorno;
+    }
+
+    /**
+     * Estabelece a conexão com o banco de dados.
+     *
+     * @return \MySQLi
+     * @throws \Exception
+     */
+    private function conectarBanco() {
+        if (!defined('DS')) {
+            define('DS', DIRECTORY_SEPARATOR);
+        }
+        if (!defined('BASE_DIR')) {
+            define('BASE_DIR', dirname(__FILE__) . DS);
+        }
+
+        require(BASE_DIR . 'configdb.php');  // Inclui as configurações do banco de dados
+
+        try {
+            $conn = new \MySQLi($dbhost, $user, $password, $banco);  // Cria a conexão
+            return $conn;
+        } catch (mysqli_sql_exception $e) {
+            throw new \Exception("Erro na conexão com o banco de dados: " . $e->getMessage());  // Lança exceção se a conexão falhar
+        }
+    }
+  
+}
 ?>
---
+++ models/recepcaoVideira.php
@@ -0,0 +1,72 @@
+<?php
+namespace models;
+
+/**
+ * Classe Model para a tabela recepcao_fraiburgo
+ * Representa os dados de recepção para a unidade Fraiburgo.
+ */
+class RecepcaoVideira {
+    /**
+     * ID do registro
+     * @var int
+     */
+    public $id;
+
+    /**
+     * Data do registro
+     * @var string (formato YYYY-MM-DD)
+     */
+    public $data;
+
+    /**
+     * Quantidade de clínicos atendidos
+     * @var int
+     */
+    public $clinico;
+
+    /**
+     * Quantidade de exames de audiometria realizados
+     * @var int
+     */
+    public $audiometria;
+
+    /**
+     * Quantidade de exames de espirometria realizados
+     * @var int
+     */
+    public $espirometria;
+
+    /**
+     * Quantidade de exames de acuidade realizados
+     * @var int
+     */
+    public $acuidade;
+
+    /**
+     * Quantidade de exames de ECG realizados
+     * @var int
+     */
+    public $ecg;
+
+    /**
+     * Quantidade de exames de EEG realizados
+     * @var int
+     */
+    public $eeg;
+
+    /**
+     * Construtor da classe RecepcaoFraiburgo
+     * Inicializa os dados de um registro.
+     */
+    public function __construct($id, $data, $clinico, $audiometria, $espirometria, $acuidade, $ecg, $eeg) {
+        $this->id = $id;
+        $this->data = $data;
+        $this->clinico = $clinico;
+        $this->audiometria = $audiometria;
+        $this->espirometria = $espirometria;
+        $this->acuidade = $acuidade;
+        $this->ecg = $ecg;
+        $this->eeg = $eeg;
+    }
+}
+?>
